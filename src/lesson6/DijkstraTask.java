631f9d5d42d9d9a9484adf29a5eb35d2360a4fc6
Alexander Chernomyrdin <chav1961@mail.ru>
2020-10-21 18:10:33 +0500

    DAily commit

---
+++ src/lesson6/DijkstraTask.java
@@ -0,0 +1,22 @@
+package lesson6;
+
+public class DijkstraTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 5 chinese eating philosofists
+	}
+	
+	
+	static void eating() {
+		try{Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+	}
+
+	static void thinking() {
+		try{Thread.sleep(5000);
+		} catch (InterruptedException e) {
+		}
+	}
+}
