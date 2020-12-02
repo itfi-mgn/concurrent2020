4fcbf4d51c1bac2fba684d46181525e562b4e1f2
Alexander Chernomyrdin <chav1961@mail.ru>
2020-12-02 18:17:32 +0500

    DAily commit

--- src/lesson10/HazelcastTest.java
+++ src/lesson10/HazelcastTest.java
@@ -1,9 +1,15 @@
 package lesson10;
 
+import java.util.EventListener;
+
 import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.EntryEvent;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
+import com.hazelcast.map.MapEvent;
+import com.hazelcast.map.impl.MapListenerAdapter;
+import com.hazelcast.map.listener.MapListener;
 
 public class HazelcastTest {
 
@@ -16,6 +22,19 @@ public class HazelcastTest {
 	        // Get the Distributed Map from Cluster.
 	        System.out.println("Getting map...");
 	        IMap map = hz.getMap("my-distributed-map");
+	        map.addEntryListener(new MapListenerAdapter(){
+	        	@Override
+		        public void onEntryEvent(EntryEvent event) {
+		        	super.onEntryEvent(event);
+		        	System.out.println("Entry="+event);
+		        }
+	        	
+	        	@Override
+        		public void onMapEvent(MapEvent event) {
+        			super.onMapEvent(event);
+		        	System.out.println("Map="+event);
+        		}
+	        }, true);
 	        //Standard Put and Get.
 	        System.out.println("Updating map...");
 	        map.put("key", "value");
@@ -29,6 +48,13 @@ public class HazelcastTest {
 	        hzInstance1.shutdown();
 	        hzInstance2.shutdown();
 		}
+		
+		// c:/x.txt
+		// ivanov
+		// petrov
+		// sidorov
+		// ivanov
+		
 	}
 
 }
---
+++ src/lesson11/QueueAndTopikTest.java
@@ -0,0 +1,54 @@
+package lesson11;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.collection.IQueue;
+import com.hazelcast.collection.ItemEvent;
+import com.hazelcast.collection.ItemListener;
+import com.hazelcast.core.EntryEvent;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+import com.hazelcast.map.MapEvent;
+import com.hazelcast.map.impl.MapListenerAdapter;
+import com.hazelcast.topic.ITopic;
+import com.hazelcast.topic.Message;
+import com.hazelcast.topic.MessageListener;
+
+public class QueueAndTopikTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HazelcastInstance hzInstance = Hazelcast.newHazelcastInstance();
+        HazelcastInstance hz = HazelcastClient.newHazelcastClient();
+		try {
+			IQueue<String>	q = hz.getQueue("testqueue");
+	        q.addItemListener(new ItemListener<String>() {
+				@Override
+				public void itemRemoved(ItemEvent<String> arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void itemAdded(ItemEvent<String> arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+			}, true);
+	        
+	        ITopic<String> topic = hz.getTopic("testtopic");
+	        topic.addMessageListener(new MessageListener<String>() {
+				@Override
+				public void onMessage(Message<String> arg0) {	// q.take()
+					// TODO Auto-generated method stub
+					
+				}
+			});
+	        topic.publish("sdsdsd");	// q.put()
+		} finally {
+	        hz.shutdown();
+	        hzInstance.shutdown();
+		}
+	}
+
+}
